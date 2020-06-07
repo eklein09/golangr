7f382c731cdcbcf4566768375dfc25fcf387ddac
Eric Klein <eric.klein@workday.com>
2020-06-07 05:39:28 -0600

    Autoformat

--- variables/calc_year.go
+++ variables/calc_year.go
@@ -15,14 +15,13 @@ func main() {
 	birthYear = birthYear[:len(birthYear)-1]
 	int_birthYear, _ := strconv.Atoi(birthYear)
 
-
 	fmt.Println("Enter your age:")
 	age, _ := reader.ReadString('\n')
 
 	age = age[:len(age)-1]
 	int_age, _ := strconv.Atoi(age)
-	fmt.Printf("Entered birth year was %d\n",int_birthYear)
-	fmt.Printf("Entered age was %d\n",int_age)
+	fmt.Printf("Entered birth year was %d\n", int_birthYear)
+	fmt.Printf("Entered age was %d\n", int_age)
 
 	currentYear := int_birthYear + int_age
 	fmt.Printf("The current year is %d\n", currentYear)
